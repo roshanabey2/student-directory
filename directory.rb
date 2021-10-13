a3a6b6a2e343170c0ffaa960a6ebe2894bdafb3a
roshanabey2 <roshanabey2@gmail.com>
2021-10-13 18:00:57 +0100

    Added the students_input method to manually add student when file is executed

--- directory.rb
+++ directory.rb
@@ -1,22 +1,27 @@
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-  ]
+
+def input_students
+    puts "Please enter the name of the student"
+    puts "To finish just presss enter twice"
+    #below we create an empty array
+    students = []
+    name = gets.chomp
+    #a loop that repeats as long name is not empty
+    until name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    # return the array of students
+    students
+end
+
+    
 def print_header
     puts "The students of Villains Academy"
     puts "-------------"
 end
 
-def print(sudents)
+def print(students)
     students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end   
 
@@ -24,6 +29,8 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students"
 end
 
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
