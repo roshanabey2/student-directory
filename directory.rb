9b456da269c827c1d59210305e2146a2526cf684
roshanabey2 <roshanabey2@gmail.com>
2021-10-13 17:35:14 +0100

    Added 3 print methods to the file directory.rb and called those mehods at the end of it

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 students = [
     "Dr. Hannibal Lecter",
     "Darth Vader",
@@ -12,8 +11,19 @@ students = [
     "Joffery Baratheon",
     "Norman Bates",
 ]
-puts "The students of Villains Academy"
-puts "-------------"
-students.each {|student| puts student}
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
+end
 
-puts "Overall, we have #{student_count} great students"
+def print(names)
+    names.each {|name| puts name }
+end   
+
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
