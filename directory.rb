ced70f0718b3d026af2c3742fa17046756f0078e
roshanabey2 <roshanabey2@gmail.com>
2021-10-13 17:26:04 +0100

    changed the code for directory.rb to be less repetive

--- directory.rb
+++ directory.rb
@@ -1,16 +1,19 @@
+student_count = 11
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex DeLard",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffery Baratheon",
+    "Norman Bates",
+]
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall, we have "
-print 11
-puts " great students"
+students.each {|student| puts student}
+
+puts "Overall, we have #{student_count} great students"
