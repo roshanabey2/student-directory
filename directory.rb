c4c6b9cd28f71038d97a1833bb66d143c0b05db0
roshanabey2 <roshanabey2@gmail.com>
2021-10-15 15:05:36 +0100

    Saved changes to the directory.rb so that it so it contains save_student method

--- directory.rb
+++ directory.rb
@@ -1,18 +1,29 @@
+@students = []
 
 def input_students
     puts "Please enter the name of the student"
     puts "To finish just presss enter twice"
-    #below we create an empty array
-    students = []
     name = gets.chomp
     #a loop that repeats as long name is not empty
     until name.empty? do
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
         name = gets.chomp
     end
     # return the array of students
-    students
+    @students
+end
+
+def save_students
+    # open the file for writing
+    file = File.open("students.csv", "w")
+    # iterate over the array of students
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
+    file.close
 end
 
     
@@ -21,8 +32,8 @@ def print_header
     puts "-------------"
 end
 
-def print(students)
-    students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+def print_student_list
+    @students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end   
 
 def print_footer(names)
@@ -32,28 +43,35 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit" # 9 because we'll be adding more items  
-  end
+end
 
+def show_students
+    print_header
+    print_student_list
+    print_footer(@students)
+end
+
+def proccess(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students
+    when "3"
+        save_students
+    when "9"
+        exit
+      else
+        puts "I don't know what you mean, try again"
+    end
+end
 
 def interactive_menu
-    students = []
     loop do
         print_menu
-        # 2. puts input into selection input variable
-        input = gets.chomp
-        case input
-        when "1"
-          students = input_students
-        when "2"
-          print_header
-          print(students)
-          print_footer(students)
-        when "9"
-          exit # this will cause the program to terminate
-        else
-          puts "I don't know what you meant, try again"
-        end
+        proccess(gets.chomp)
     end
 end
 
